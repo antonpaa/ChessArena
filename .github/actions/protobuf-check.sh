a2aa58294a3d4860602fd6ac2ba489d405d3917f
Anton Paakkonen <anton.paakkonen@fi.abb.com>
2020-11-01 03:42:24 +0200

    Add python3 aliases

--- .github/actions/protobuf-check.sh
+++ .github/actions/protobuf-check.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-whereis python3
+alias python3='/usr/bin/python3.7'
 python3 -m grpc_tools.protoc -I ./Server/grpc/protos --python_out=. --grpc_python_out=. ./Server/grpc/protos/GameManager.proto
